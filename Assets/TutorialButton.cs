d50c0ef7de8d05c2008138e53853ff71d4f555a9
masahiro_mac <okamok108@gmail.com>
2018-01-14 14:47:05 +0900

    tutorialを追加 音声、デフォルトでOFFにした

---
+++ Assets/TutorialButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialButton : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR
+		PlayerPrefs.DeleteKey("Init"); 
+		#elif UNITY_ANDROID
+
+		#endif
+	}
+
+	//	// Update is called once per frame
+	//	void Update () {
+	//		
+	//	}
+
+	public void OnClickFinishTurorial() {
+		Debug.Log("OnClickFinishTurorial click!");
+		GameObject TutorialUI = GameObject.Find ("TutorialUI");
+		TutorialUI.SetActive (false);
+
+		//チュートリアル終了 フラグ
+		PlayerPrefs.SetInt("Init", 1); 
+	}
+
+	public void OnClickAccessHomepage() {
+		Debug.Log("OnAccessHomepage click!");
+		Application.OpenURL("https://universear.hiliberate.biz/");
+	}
+
+}
